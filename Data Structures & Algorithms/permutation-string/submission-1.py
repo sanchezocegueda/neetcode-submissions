33a29de2db803d23f0a0288ca4ed04985f9d92b6
Alex <96963333+sanchezocegueda@users.noreply.github.com>
2026-05-31 13:33:45 -0700

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1 = len(s1)
+        n2 = len(s2)
+        f = defaultdict(int)
+        chars = set(s1)
+
+        for c in s1:
+            f[c] += 1
+
+        l = 0
+        rem = n1
+        for r in range(n2):
+            cr = s2[r]
+            f[cr] -= 1
+            if cr in chars and f[cr] >= 0:
+                rem -= 1
+            
+            # Window at size
+            if r - l + 1 > n1:
+                cl = s2[l]
+                f[cl] += 1 # leaving this one behind
+                if cl in chars and f[cl] > 0:
+                    rem += 1
+                l += 1
+            
+            # new window at this point
+            if rem == 0:
+                return True
+            
+        return False
+
