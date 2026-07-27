c470ea287e2e3ee86ddd1bbe95d7b01be39e5346
Alex <96963333+sanchezocegueda@users.noreply.github.com>
2026-07-27 03:09:52 -0700

    Add: majority-element - submission-4

---
+++ Data Structures & Algorithms/majority-element/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # Boyer-Moore algorithm
+
+        candidate = nums[0]
+        count = 0
+
+        for num in nums:
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+            if count == 0:
+                candidate = num
+                count = 1
+
+        return candidate
+            
+            
