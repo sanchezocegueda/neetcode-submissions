51fc523090444d0ff60ea9bbe0b21dffc2710381
Alex <96963333+sanchezocegueda@users.noreply.github.com>
2026-08-14 01:42:23 -0700

    Add: two-sum-bsts - submission-1

---
+++ Data Structures & Algorithms/two-sum-bsts/submission-1.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
+        # use two-sum hash map approach
+        # use bst property to avoid traversing the whole tree
+
+        complements = set()
+
+
+        def dfs(node):
+
+            if node is None:
+                return False
+        
+            if bst_search(root2, target-node.val):
+                return True
+            
+            return dfs(node.left) or dfs(node.right)
+
+
+        def bst_search(node, target):
+            if node is None:
+                return False
+            
+            if node.val == target:
+                return True
+            
+            if target < node.val:
+                return bst_search(node.left, target)
+            else:
+                return bst_search(node.right, target)
+
+        return dfs(root1)
+            
