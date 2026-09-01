641ab21c8b214b274fafe52ed1fd3746fdc557a5
Alex <96963333+sanchezocegueda@users.noreply.github.com>
2026-09-01 14:11:45 -0700

    Add: target-sum - submission-4

---
+++ Data Structures & Algorithms/target-sum/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        # brute force solution
+
+
+        n = len(nums)
+        dp = Counter() # cache the sums?
+        dp[0] = 1
+
+        for i in range(n):
+            newDP = Counter()
+            for s in dp.keys():
+                plus = s + nums[i]
+                minus = s - nums[i]
+                
+                newDP[plus] += dp[s]
+                newDP[minus] += dp[s]
+            dp = newDP
+
+        return dp[target]
+        
