bb3659f0a829dc1245c911c5719c5e7d8cbe2708
Alex <96963333+sanchezocegueda@users.noreply.github.com>
2026-09-02 12:31:48 -0700

    Add: find-first-and-last-position-of-element-in-sorted-array - submission-6

---
+++ Data Structures & Algorithms/find-first-and-last-position-of-element-in-sorted-array/submission-6.py
@@ -0,0 +1,27 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        # approach: find lower bound (first occurrence) of target and target + 1
+        # say L is lower bound of target and R is lower bound of target + 1
+        # then [L, R) is the desired range
+        if not nums:
+            return [-1, -1]
+
+
+        def lower_bound(target):
+            l, r = 0, len(nums)
+
+            while l < r:
+                m = l + (r-l) // 2
+
+                if nums[m] < target:
+                    l = m + 1
+                else:
+                    r = m
+            return l
+
+        L, R = lower_bound(target), lower_bound(target+1)
+
+        if L == len(nums) or nums[L] != target:
+            return [-1, -1]
+        
+        return [L, R-1]
