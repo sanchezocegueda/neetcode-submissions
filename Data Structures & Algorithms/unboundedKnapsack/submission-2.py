1e6a149cc9a94c1ba05d9b192f4cbc3517c4a969
Alex <96963333+sanchezocegueda@users.noreply.github.com>
2026-09-03 18:59:34 -0700

    Add: unboundedKnapsack - submission-2

---
+++ Data Structures & Algorithms/unboundedKnapsack/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximumProfit(self, profit: List[int], weight: List[int], capacity: int) -> int:
+        m, n = capacity, len(profit)
+
+        dp = [[0] * (m+1) for _ in range(n)]
+
+        for c in range(m+1):
+            if weight[0] <= c:
+                dp[0][c] = (c // weight[0]) * profit[0] # however many we can grab
+            
+        
+        for i in range(1, n):
+            for c in range(1, m+1):
+                skip = dp[i-1][c]
+
+
+                incl = 0
+                if c - weight[i] >= 0:
+                    incl = profit[i] + dp[i][c-weight[i]]
+                
+                dp[i][c] = max(skip, incl)
+        
+        return dp[n-1][m]
